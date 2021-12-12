6e328b9576584813bb000700b2f3b4a4c57f3f63
Sunny <30768018+sunny52525@users.noreply.github.com>
2021-12-12 16:56:44 +0530

    added canvas api

--- CanvasApi/app/src/main/java/com/gfg/canvasapi/MainActivity.kt
+++ CanvasApi/app/src/main/java/com/gfg/canvasapi/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.gfg.canvasapi
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
@@ -21,73 +18,87 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gfg.canvasapi.ui.theme.CanvasAPITheme
-import com.gfg.canvasapi.ui.theme.GFGGreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CanvasAPITheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    GFGIcon()
-                }
+          GFGIcon()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GFGIcon() {
+    CanvasAPITheme {
+        //Create a box to and set contentAlignment to Center to  center the Icon
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFF308D46)),
+
+            contentAlignment = Alignment.Center
+        ) {
+
+            //Create a row
+            Row {
+
+                //Place one Icon in the row but rotate Y Axis by -180 degree
+                // it will look like mirror image
+                Icon(Modifier.graphicsLayer(rotationY = -180f))
+                //set some space between the icons
+                Spacer(modifier = Modifier.width(20.dp))
+
+                // Original Icon without rotation
+                Icon()
+
             }
         }
     }
 }
 
 @Composable
-fun GFGIcon() {
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(GFGGreen),
-        contentAlignment = Alignment.Center
-    ) {
-        Row() {
-            Icon(Modifier.graphicsLayer {
-                rotationY=-180f
-            })
-            Spacer(modifier = Modifier.width(15.dp))
-            Icon()
-
-        }
-    }
-}
-
-@Composable
 fun Icon(modifier: Modifier = Modifier) {
+
+    /**
+     * Draw the Canvas Composable
+     */
     Canvas(modifier = modifier.size(100.dp), onDraw = {
+        /**
+         * Canvas scope provides width and height of the canvas
+         * save height and width of canvas in a variable to use in the code
+         */
         val canvasWidth = size.width
         val canvasHeight = size.height
 
+        //we first draw the arc which will be the curve of the logo
         drawArc(
             color = Color.White,
+            //arc starts at 0 degree and ends
             startAngle = 0f,
-            useCenter = true,
-            sweepAngle = 310f,
-            style = Stroke(width = 54f, cap = StrokeCap.Round)
+            //set use center to false to draw the arc without centered line
+            //Tip: use center to true to draw the arc with centered line and see the difference
+            useCenter = false,
+            //set the end angle of the arc
+            sweepAngle = 290f,
+            //set the width of the arc and how the arc cap will be drawn
+            //  cap = StrokeCap.Round will draw the arc with rounded end
+            style = Stroke(width = 40f, cap = StrokeCap.Square)
         )
-        drawCircle(
-            color = GFGGreen,
-            radius = 110f
-        )
+
+        //draw the center line of the logo
         drawLine(
+
             color = Color.White,
+            //set the start point of the line to the center of the canvas
             start = Offset(x = 0f, y = canvasHeight / 2),
+            //set the end point of the line to the center of the canvas
             end = Offset(x = canvasWidth, y = canvasHeight / 2),
-            strokeWidth = 54f
+            //set the width of the line
+            strokeWidth = 40f
         )
 
     })
 
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CanvasAPITheme {
-        GFGIcon()
-    }
 }
--- CanvasApi/app/src/main/java/com/gfg/canvasapi/ui/theme/Color.kt
+++ CanvasApi/app/src/main/java/com/gfg/canvasapi/ui/theme/Color.kt
@@ -2,9 +2,9 @@ package com.gfg.canvasapi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Purple200 = Color(0xFF308D46)
+val Purple500 = Color(0xFF308D46)
+val Purple700 = Color(0xFF308D46)
 val Teal200 = Color(0xFF03DAC5)
 
 val GFGGreen = Color(0xFF308D46)
